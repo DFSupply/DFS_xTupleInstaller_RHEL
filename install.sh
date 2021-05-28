32aac69816324f768c308bff0d68946ffb3fdd28
Scott Moore <scott@discountfence.com>
2021-05-27 20:08:21 -0400

    install Libc++abi prerequisite to compile PLV8

--- install.sh
+++ install.sh
@@ -47,11 +47,18 @@ postgresql-setup initdb
 echo "PLV8 compilation prereqs"
 subscription-manager repos --enable=codeready-builder-for-rhel-8-x86_64-rpms
 yum update -y
-yum install git python2 python3 gcc pkg-config ninja-build make -y
+yum install git python2 python3 gcc pkg-config ninja-build make ncurses-compat-libs redhat-rpm-config clang cmake llvm-devel -y
 # need python 2 for compilation
 alternatives --set python /usr/bin/python2
-echo "Compiling PLV8"
 
+echo "Compiling libc++abi"
+git clone https://github.com/llvm/llvm-project.git llvm-project
+cd llvm-project
+mkdir build && cd build
+cmake -DLLVM_ENABLE_PROJECTS=libcxxabi ../llvm  -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++
+make
+
+echo "Compiling PLV8"
 wget https://github.com/plv8/plv8/archive/v2.3.15.tar.gz
 tar -xzvf v2.3.15.tar.gz
 cd plv8-2.3.15
