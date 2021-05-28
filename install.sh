6fce66f9190bd1ffe66a3a5a0e2c2252026c3f0b
Scott Moore <scott@discountfence.com>
2021-05-28 12:38:46 -0400

    cleanup status messages

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# WARNING NOT FINISHED!!!!
+# DO NOT USE IN PRODUCTION
+
 # Requires:
 # Red Hat Enterprise Linux 8.x
 # A working internet connection.
@@ -40,7 +43,7 @@ echo ""
 echo "PostgreSQL version $PG_VER on port $PG_PORT"
 echo "$XT_ADMIN / $XT_ADMIN_PASS / $XT_ROLE"
 
-echo "Switch PostgreSQL streams and install"
+echo "Switching PostgreSQL streams and installing..."
 yum update -y
 yum module reset postgresql -y
 yum module enable postgresql:${PG_VER} -y
@@ -50,7 +53,7 @@ echo "Initializing..."
 postgresql-setup initdb
 
 
-echo "PLV8 compilation prereqs"
+echo "PLV8 compilation prereqs..."
 subscription-manager repos --enable=codeready-builder-for-rhel-8-x86_64-rpms
 yum update -y
 yum install git python2 python3 gcc pkg-config ninja-build make ncurses-compat-libs redhat-rpm-config clang cmake llvm-devel libatomic libstdc++ -y
@@ -58,7 +61,7 @@ yum install git python2 python3 gcc pkg-config ninja-build make ncurses-compat-l
 alternatives --set python /usr/bin/python2
 
 
-echo "Compiling libcxx"
+echo "Compiling libcxx..."
 git clone -b llvmorg-12.0.0 https://github.com/llvm/llvm-project.git llvm-project
 cd llvm-project || exit
 cd libcxx || exit
@@ -67,7 +70,7 @@ cmake ../ -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER
 make || exit
 cd ../../ || exit
 
-echo "Compiling libcxxabi"
+echo "Compiling libcxxabi..."
 cd libcxxabi || exit
 mkdir build && cd build || exit
 cmake ../ -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DLIBCXX_CXX_ABI=libstdc++ -DLIBCXXABI_LIBCXX_INCLUDES=../../libcxx/include || exit
@@ -75,7 +78,7 @@ make || exit
 make install || exit
 cd ../../ || exit
 
-echo "Compiling libcxx again w/ libcxxabi"
+echo "Compiling libcxx again w/ libcxxabi..."
 cd libcxx || exit
 rm -Rf build
 mkdir build && cd build || exit
@@ -84,13 +87,13 @@ make || exit
 make install || exit
 cd ../../../ || exit
 
-echo "Compiling PLV8"
+echo "Compiling PLV8..."
 git clone -b v2.3.15 https://github.com/plv8/plv8.git plv8-2.3.15
 cd plv8-2.3.15 || exit
 make || exit
 make install || exit
 cd ../ || exit
 
-echo "Linking to LD Library"
+echo "Linking to LD Library..."
 echo "/usr/local/lib/" > /etc/ld.so.conf.d/plv8.conf.d || exit
 ldconfig
